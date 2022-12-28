ef9217d58e3b75909c456037814d36854346facd
federedef <federogc98@gmail.com>
2022-12-28 19:40:57 +0100

    test and scripts migrated, with all tests corrects

---
+++ bin/ranker.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+
+import argparse
+import sys
+import os
+import glob
+import numpy as np
+ROOT_PATH=os.path.dirname(__file__)
+sys.path.append(os.path.join(ROOT_PATH, '..'))
+
+from NetAnalyzer import Ranker
+
+########################### METHODS ########################
+#############################################################
+
+def write_ranking(file, ranking_list):
+  with open(file, 'w') as f:
+    for seed_name, ranking in ranking_list.items():
+      for ranked_gene in ranking:
+        f.write('\t'.join(map(str,ranked_gene)) + "\t" + f"{seed_name}" + "\n")     
+
+
+########################### OPTPARSE ########################
+#############################################################
+
+def threads_based_0(trheads): return int(threads) - 1
+
+parser = argparse.ArgumentParser(description='Get the ranks from a matrix similarity score and a list of seeds')
+parser.add_argument("-k", "--input_kernels", dest="kernel_file", default=None, 
+  help="The roots from each kernel to integrate")
+parser.add_argument("-n", "--input_nodes", dest="input_nodes", default=None,
+  help="The list of node for each kernel in lst format")
+parser.add_argument("-s", "--genes_seed", dest="genes_seed", default=None,
+  help="The name of the gene to look for backups")
+parser.add_argument("-S", "--seed_sep", dest="seed_sep", default=",",
+  help="Separator of seed genes. Only use when -s point to a file")
+parser.add_argument("-f", "--filter", dest="filter", default=None,
+  help="PATH to file with seed_name and genes to keep in output")
+parser.add_argument("-l", "--leave_one_out", dest="leave_one_out", default=False, 
+  action='store_true', help="PATH to file with seed_name and genes to keep in output")
+parser.add_argument("-t", "--top_n", dest="top_n", default=None, type=int,
+  help="Top N genes to print in output")
+parser.add_argument("--output_top", dest="output_top", default=None,
+  help="File to save Top N genes")
+parser.add_argument("-o", "--output_name", dest="output_name", default="ranked_genes",
+ help="PATH to file with seed_name and genes to keep in output")
+parser.add_argument("-T", "--threads", dest="threads", default=0, type=threads_based_0,
+ help="Number of threads to use in computation, one thread will be reserved as manager.")
+options = parser.parse_args()
+
+
+########################### MAIN ############################
+#############################################################
+
+ranker = Ranker()
+ranker.matrix = np.load(options.kernel_file)
+ranker.load_nodes_from_file(options.input_nodes)
+ranker.load_seeds(options.genes_seed, sep= options.seed_sep)
+options.filter is not None and ranker.load_references(options.filter, sep= ",") 
+ranker.do_ranking(leave_one_out= options.leave_one_out, threads= options.threads)
+rankings = ranker.ranking
+
+discarded_seeds = [seed_name for seed_name, ranks in rankings.items() if not ranks]
+
+if discarded_seeds:
+  with open(options.output_name + "_discarded","w") as f:
+    for seed_name in discarded_seeds:
+      f.write(f"{seed_name}\t{options[:seed_sep].join(ranker.seeds[seed_name])}")
+
+if options.top_n is not None:
+  top_n = ranker.get_top(options.top_n)
+  if options.output_top is None:
+    rankings = top_n
+  else:
+    write_ranking(options.output_top, top_n)
+
+if options.filter is not None:
+  rankings = ranker.get_reference_ranks()
+
+if rankings:
+  write_ranking(f"{options.output_name}_all_candidates", rankings)
---
+++ test_scripts/ranker_script_tests.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+source ~soft_bio_267/initializes/init_python
+export PATH=../bin/:$PATH
+test_data=../test/data/data_ranker
+out=output_test_scripts 
+data_to_test=data_test_scripts
+mkdir $out
+
+
+# ranker by seeds -----------------------------------------------------------------------------------------------------------------------------------------------
+##string
+echo "$out/ranker_by_seed_string_results"
+ranker.py -k $test_data/kernel_for_validating -n $test_data/kernel_for_validating.lst -s 'A,B' -o $out/ranker_by_seed_string_results
+## file
+ranker.py -k $test_data/kernel_for_validating -n $test_data/kernel_for_validating.lst -s $test_data/seed_genes_for_validating -o $out/ranker_by_seed_file_results
+
+# ranker leaving one out ----------------------------------------------------------------------------------------------------------------------------------------
+ranker.py -k $test_data/kernel_for_validating -n $test_data/kernel_for_validating.lst -s $test_data/seed_genes_for_validating -l -o $out/ranker_leave_one_out_by_seed_results
+
+# ranker with filter --------------------------------------------------------------------------------------------------------------------------------------------
+ranker.py -k $test_data/kernel_for_validating -n $test_data/kernel_for_validating.lst -s $test_data/seed_genes_for_validating -f $test_data/genes2filter_for_validating -o $out/ranker_filter_results
+
+# ranker with top=2 ---------------------------------------------------------------------------------------------------------------------------------------------
+ranker.py -k $test_data/kernel_for_validating -n $test_data/kernel_for_validating.lst -s $test_data/seed_genes_for_validating -t '2' --output_top $out/ranker_top_results
+
+
+for file_to_test in `ls $out`; do
+	echo $file_to_test
+	diff $out/$file_to_test $data_to_test/$file_to_test"_to_test"
+done
+

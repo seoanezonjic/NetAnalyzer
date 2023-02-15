7e50497b676562852c551a0942f8902a37ce358b
federedef <federogc98@gmail.com>
2023-02-15 17:34:44 +0100

    finishing text2binary_matrix migration and testing

--- bin/text2binary_matrix.py
+++ bin/text2binary_matrix.py
@@ -10,19 +10,18 @@ import numpy as np
 ## METHODS
 #############################################################################
 
-def load_matrix_file(input_file, splitChar = "\t"):
+def load_matrix_file(source, splitChar = "\t"):
 	matrix = None
 	counter = 0
-	with open(input_file) as f:
-		for line in f:
-	    	line = line.strip()
-    		row = [float(c) for c in line.split(splitChar)]
-    		if matrix is None:
-    			matrix = np.zeros((len(row), len(row)))
-    		for i, val in enumerate(row):
-    			matrix[counter, i] = val 
-    		
-    		counter += 1
+	for line in source:
+		line = line.strip()
+		
+		row = [float(c) for c in line.split(splitChar)]
+		if matrix is None:
+			matrix = np.zeros((len(row), len(row)))
+		for i, val in enumerate(row):
+			matrix[counter, i] = val 	
+		counter += 1
 	return matrix
 
 def load_pair_file(source, byte_format = "float32"):
@@ -41,12 +40,12 @@ def add_pair(node_a, node_b, weight, connections):
 	query = connections.get(node_a)
 	if query is not None:
 		query[node_b] = weight
-	else
+	else:
 		subhash = {}
 		subhash[node_b] = weight
 		connections[node_a] = subhash
 
-def get_stats(matrix)
+def get_stats(matrix):
 	stats = []
 	#TODO: trnasform to Numo::Array operations
 	primary_stats = get_primary_stats(matrix)
@@ -81,7 +80,7 @@ def get_stats(matrix)
 	stats.append(['Edges - Variance', connection_stats["variance"]])
 	stats.append(['Edges - Standard Deviation', connection_stats["standardDeviation"]])
 	stats.append(['Edges - Q1', connection_stats["q1"]])
-	stats.append(['Edges - Median', connection_stats["q2"]])
+	stats.append(['Edges - Median', connection_stats["median"]])
 	stats.append(['Edges - Q3', connection_stats["q3"]])
 	stats.append(['Edges - Min Non Zero', connection_stats["minNonZero"]])
 	stats.append(['Edges - Average Non Zero', connection_stats["averageNonZero"]])
@@ -90,6 +89,8 @@ def get_stats(matrix)
 	stats.append(['Edges - Q1 Non Zero', connection_stats["q1NonZero"]])
 	stats.append(['Edges - Median Non Zero', connection_stats["medianNonZero"]])
 	stats.append(['Edges - Q3 Non Zero', connection_stats["q3NonZero"]])
+
+	stats = map(lambda x: [x[0],str(x[1])],stats)
 	
 	return stats
 
@@ -101,7 +102,7 @@ def get_connection_number(matrix):
 		column = matrix[:, i] 
 		count = 0
 		for value in column:
-			count += 1 if value != 0
+			count += 1 if value != 0 else 0
 
 		connections[0, i] = count - 1 # the connection with self is removed
 
@@ -109,38 +110,41 @@ def get_connection_number(matrix):
 
 def transform_keys(hash, function):
 	new_hash = {}
-	for key, val in items(hash):
+	for key, val in hash.items():
 		new_key = function(key)
 		new_hash[new_key] = val
 
 	return new_hash
 
 def get_primary_stats(matrix):
-	stats = Hash.new(0)
+	stats = {}
 	max = matrix[0, 0] # Initialize max value
 	min = matrix[0, 0] # Initialize min value
 	min_non_zero = matrix[0, 0] # Initialize min value
 
 	values = matrix.flatten()
 
-	for value in matrix:
+	stats["count"] = 0
+	stats["countNonZero"] = 0 
+	stats["sum"] = 0
+	for value in values:
 		stats["count"] += 1
-		stats["countNonZero"] += 1 if value != 0
+		stats["countNonZero"] += 1 if value != 0 else 0
 		stats["sum"] += value
-		max = value if value > max
-		min = value if value < min
-		min_non_zero = value if value != 0 and value < min
+		max = value if value > max else max
+		min = value if value < min else min
+		if value != 0 and value < min:
+			min_non_zero = value
 	
 	stats["max"] = max
 	stats["min"] = min
 	stats["minNonZero"] = min_non_zero
 
-	#values.flatten! if values.first.class == Array
 	quartile_stats = get_quartiles(values, stats["count"])
 	stats.update(quartile_stats)
-	values = [v for v in values if v != 0]
-	quartile_stats_non_zero = get_quartiles(values, stats[:countNonZero])
-	stats.update(quartile_stats_non_zero)
+	non_zero_values = [v for v in values if v != 0]
+	quartile_stats_non_zero = get_quartiles(non_zero_values, stats["countNonZero"])
+	stats.update(transform_keys(quartile_stats_non_zero, lambda x: x + "NonZero"))
 	get_composed_stats(stats, values)
 	return stats
 
@@ -157,9 +161,11 @@ def get_composed_stats(stats, values):
 	stats["average"] = average
 	stats["averageNonZero"] = average_non_zero
 
+	stats["sumDevs"] = 0
+	stats["sumDevsNonZero"] = 0
 	for value in values:
-		stats["sumDevs"] = (value - average) ** 2
-		stats["sumDevsNonZero"] = (value - average_non_zero) ** 2 if value != 0
+		stats["sumDevs"] += (value - average) ** 2
+		stats["sumDevsNonZero"] += (value - average_non_zero) ** 2 if value != 0 else 0
 
 	stats["variance"] = stats["sumDevs"]/stats["count"]
 	stats["varianceNonZero"] = stats["sumDevsNonZero"]/stats["countNonZero"]
@@ -170,7 +176,7 @@ def get_composed_stats(stats, values):
 ## OPTPARSE
 #############################################################################
 
-parser = parser.ArgumentParser(description="Transforming matrix format and obtaining statistics")
+parser = argparse.ArgumentParser(description="Transforming matrix format and obtaining statistics")
 
 parser.add_argument('-i', '--input_file', dest="input_file", default=None,
 	help="input file")
@@ -179,7 +185,7 @@ parser.add_argument('-o', '--output_matrix_file', dest="output_matrix_file", def
 parser.add_argument('-b', '--byte_format', dest="byte_format", default="float64",
 	help='Format of the numeric values stored in matrix. Default: float64, warning set this to less precission can modify computation results using this matrix.')
 parser.add_argument('-t', '--input_type', dest="input_type", default='pair',
-	help='Set input format file. "pair" or "matrix"')
+	help='Set input format file. "pair", "matrix" or "bin"')
 parser.add_argument('-d', '--set_diagonal', dest="set_diagonal", default=False, action='store_true',
 	help='Set to 1.0 the main diagonal')
 parser.add_argument('-B', '--binarize', dest="binarize", default=None, type = lambda x: float(x),
@@ -196,20 +202,22 @@ options = parser.parse_args()
 ## MAIN
 ###############################################################################
 if options.input_file == '-':
-	source = STDIN # TODO(Fred): see ths part to python
+	source = sys.stdin 
 else:
-	source = File.open(options.input_file) # TODO(Fred): see ths part to python
+	source = open(options.input_file) 
 
 if options.input_type == 'bin':
 	matrix = np.load(options.input_file)
-	#matrix = Marshal.load(File.binread(options[:input_file])) # the method needs a path not a IO object 
 elif options.input_type == 'matrix':
 	matrix = load_matrix_file(source)
 elif options.input_type == 'pair':
 	matrix, names = load_pair_file(source, options.byte_format)
-	File.open(options[:output_matrix_file]+'.lst', 'w'){|f| f.print names.join("\n")} # TODO(Fred): see ths part to python
+	with open(options.output_matrix_file + ".lst", 'w') as f:
+		f.write("\n".join(names))
 
-if options["set_diagonal"]:
+source.close()
+
+if options.set_diagonal:
 	elements = matrix.shape[-1]
 	for n in range(elements):
 		matrix[n, n] = 1.0
@@ -222,24 +230,21 @@ if options.binarize is not None and options.cutoff is None:
 			matrix[i,j] = 1 if matrix[i,j] >= options.binarize else 0 
 
 
-if options.cutoff is None and options.binarize is None:
+if options.cutoff is not None and options.binarize is None:
 	elements = matrix.shape[-1]
 	for i in range(elements):
 		for j in range(elements):
-			matrix[i,j] = matrix[i,j] if matrix[i,j] >= options[:cutoff] else 0 
+			matrix[i,j] = matrix[i,j] if matrix[i,j] >= options.cutoff else 0 
 
 
 if options.stats:
 	stats = get_stats(matrix)
 
-	for stat in stats
-		#puts stat.join("\t") # TODO(Fred): see ths part to python
+	for stat in stats:
+		print("\t".join(stat))
 
 
 if options.output_type == 'bin':	
-	#File.binwrite(options[:output_matrix_file], Marshal.dump(matrix)) # TODO(Fred): see ths part to python
 	np.save(options.output_matrix_file, matrix)
 elif options.output_type == 'mat':
-	#File.open(options[:output_matrix_file], 'w') do |f| # TODO(Fred): see ths part to python
-	#	matrix.each_over_axis(0) do |r|
-	#		f.puts r.to_a.join("\t")
+	np.savetxt(options.output_matrix_file, matrix)
---
+++ test_scripts/text2binary_matrix_tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+source ~soft_bio_267/initializes/init_python
+export PATH=../bin/:$PATH
+out=output_test_scripts/text2binary_matrix
+data_to_test=data_test_scripts/text2binary_matrix
+mkdir $out
+
+source ~soft_bio_267/initializes/init_ruby
+
+text2binary_matrix.py -i $data_to_test/test_matrix_bin.npy -t "bin" -s -o $out/output_file > $out/statistics_from_text2bin
+rm $out/output_file.npy
+
+text2binary_matrix.py -i $data_to_test/test_matrix_bin.npy -t "bin" -o $out/cutoff_no_binarizado -c 0.5
+
+text2binary_matrix.py -i $data_to_test/test_matrix_bin.npy -t "bin" -o $out/cutoff_binarizado -B 0.5
+
+text2binary_matrix.py -i $data_to_test/test_matrix_bin.npy -t "bin" -o $out/set_diagonal_matrix -d
+
+text2binary_matrix.py -i $data_to_test/test_matrix_bin.npy -t "bin" -O "mat" -o $out/test_matrix
+
+for file_to_test in `ls $out`; do
+	echo $file_to_test
+	diff $out/$file_to_test $data_to_test/$file_to_test
+done

Fixing error in cosine normalization

NetAnalyzer/adv_mat_calc.py
@@ -69,8 +69,8 @@ class Adv_mat_calc:
 	def cosine_normalization(matrix):
 		dims = np.shape(matrix)
 		normalized_matrix =  np.zeros(dims)
-		for i in range(0, dims[0] - 1):
-			for j in range(0, dims[1] - 1):
+		for i in range(0, dims[0]):
+			for j in range(0, dims[1]):
 				norm = matrix[i, j]/np.sqrt(matrix[i, i] * matrix[j,j])
 				normalized_matrix[i, j] = norm
 		return normalized_matrix
e4b6a25d962442e3c7abe15fb8464f5c79cb38e9
federedef <federogc98@gmail.com>
2023-03-28 02:10:24 +0200

    fixing minor errors in integrate bin

--- bin/integrate_kernels.py
+++ bin/integrate_kernels.py
@@ -35,25 +35,17 @@ options = parser.parse_args()
 ############################################################
 kernels = Kernels()
 
-print("------------------------------------_EY1 ------------------------")
-
 if not options.kernel_ids:
 	options.kernel_ids = list(range(0,len(options.kernel_files)))
 	options.kernel_ids = [str(k) for k in options.kernel_ids]
-
-
 
 # TODO: Consider adding more options to integrate to accept other formats
 if options.input_format == "bin":
 	kernels.load_kernels_by_bin_matrixes(options.kernel_files, options.node_files, options.kernel_ids)
 	kernels.create_general_index()
-print("------------------------------------_EY2 ------------------------")
 
 if options.integration_type is not None:
 	kernels.integrate_matrix(options.integration_type, options.n_workers)
-
-
-print("------------------------------------_EY3 ------------------------")
 
 if options.output_matrix_file is not None:
 	kernel, names = kernels.integrated_kernel
@@ -62,32 +54,4 @@ if options.output_matrix_file is not None:
 	with open(options.output_matrix_file +'.lst', 'w') as f:
 		for name in names:
 			f.write(name + "\n")  
-
-#import timeit
-#
-## Define functions that are called in the code blocks
-#def load_kernels():
-#    kernels.load_kernels_by_bin_matrixes(options.kernel_files, options.node_files, options.kernel_ids)
-#    kernels.create_general_index()
-#
-#def integrate():
-#    kernels.integrate_matrix(options.integration_type)
-#
-#def save_kernel():
-#    kernel, names = kernels.integrated_kernel
-#    np.save(options.output_matrix_file, kernel)
-#
-#    with open(options.output_matrix_file +'.lst', 'w') as f:
-#        for name in names:
-#            f.write(name + "\n")  
-#
-## Measure the execution time of the code blocks using timeit
-#t1 = timeit.timeit(load_kernels, number=1)
-#t2 = timeit.timeit(integrate, number=1)
-#t3 = timeit.timeit(save_kernel, number=1)
-#
-## Print the execution times
-#print(f'load binary matrixes: {t1} seconds')
-#print(f'Final integration: {t2} seconds')
-#print(f'Save numpy matrix: {t3} seconds')
 
